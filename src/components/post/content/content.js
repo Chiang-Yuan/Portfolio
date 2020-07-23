76417dddaf683165e8a9a29eca11240e43794e26
Yuan Chiang <qaqowotut@gmail.com>
2020-07-24 01:39:10 +0800

    Bugfix

--- src/components/post/content/content.js
+++ src/components/post/content/content.js
@@ -1,17 +1,23 @@
 // @flow strict
 import React from 'react';
+import PropTypes from 'prop-types';
 // import styles from './Content.module.scss';
 
-type Props = {
-  body: string,
-  title: string
-};
+// type Props = {
+//   body: string,
+//   title: string
+// };
 
-const Content = ({ body, title }: Props) => (
+const Content = ({ body, title }) => (
   <div>
     <h1>{title}</h1>
     <div dangerouslySetInnerHTML={{ __html: body }} />
   </div>
 );
+
+Content.propTypes = {
+  body: PropTypes.string,
+  title: PropTypes.string,
+};
 
 export default Content;
