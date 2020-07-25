513cf5ede18c925e2fc65b69ed276a5e5cfdac00
Yuan Chiang <qaqowotut@gmail.com>
2020-07-26 01:06:21 +0800

    update super blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -67,7 +67,10 @@ export default function Blog({ data }) {
 
 export const query = graphql`
   query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC}
+      filter: {fileAbsolutePath: {regex: "content/blog/"  }}
+    ){
       totalCount
       edges {
         node {
